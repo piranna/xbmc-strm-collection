5f95f7121f5d115f263ebd22475d5bcfcf2c535f
akaigotchi <akaigotchi@39f0f031-5427-0410-9fd6-272d3ce43485>
2007-02-06 21:15:57 +0000

    Updated BUILD.bat to match newly added countries

--- BUILD.bat
+++ BUILD.bat
@@ -13,17 +13,22 @@ xcopy "Brazil" "XBMC-TV\Brazil" /E /Q /I /Y
 xcopy "Canada" "XBMC-TV\Canada" /E /Q /I /Y
 xcopy "Czech Republic" "XBMC-TV\Czech Republic" /E /Q /I /Y
 xcopy "Denmark" "XBMC-TV\Denmark" /E /Q /I /Y
+xcopy "Estonia" "XBMC-TV\Estonia" /E /Q /I /Y
 xcopy "France" "XBMC-TV\France" /E /Q /I /Y
 xcopy "Germany" "XBMC-TV\Germany" /E /Q /I /Y
 xcopy "Hungary" "XBMC-TV\Hungary" /E /Q /I /Y
 xcopy "Italy" "XBMC-TV\Italy" /E /Q /I /Y
+xcopy "Latvia" "XBMC-TV\Latvia" /E /Q /I /Y
+xcopy "Lithuania" "XBMC-TV\Lithuania" /E /Q /I /Y
 xcopy "Japan" "XBMC-TV\Japan" /E /Q /I /Y
 xcopy "Luxembourg" "XBMC-TV\Luxembourg" /E /Q /I /Y
 xcopy "Netherlands" "XBMC-TV\Netherlands" /E /Q /I /Y
 xcopy "Poland" "XBMC-TV\Poland" /E /Q /I /Y
 xcopy "Portugal" "XBMC-TV\Portugal" /E /Q /I /Y
+xcopy "Romania" "XBMC-TV\Romania" /E /Q /I /Y
 xcopy "Russia" "XBMC-TV\Russia" /E /Q /I /Y
 xcopy "Slovakia" "XBMC-TV\Slovakia" /E /Q /I /Y
+xcopy "Slovenia" "XBMC-TV\Slovenia" /E /Q /I /Y
 xcopy "Sweden" "XBMC-TV\Sweden" /E /Q /I /Y
 xcopy "Switzerland" "XBMC-TV\Switzerland" /E /Q /I /Y
 xcopy "United Kingdom" "XBMC-TV\United Kingdom" /E /Q /I /Y

e2ed87a1ec36dba360a24357f46b992cd7a260aa
akaigotchi <akaigotchi@39f0f031-5427-0410-9fd6-272d3ce43485>
2007-02-14 23:36:58 +0000

    Updated BUILD.bat to match newly added countries Added folder Broken/United Kingdom Moved United Kingdom/BBC World.strm to Broken/United Kingdom/BBC World.strm Added folder Antigua and Barbados Added Antigua and Barbados/ABS.strm Added folder Argentina Added Argentina/Canal 11.strm Added Argentina/Canal 26.strm Added Argentina/Canal 6.strm Added Argentina/TN 24 Horas.strm Added Argentina/TV Uno.strm Added folder Chile Added Chile/Chilevision.strm Added Chile/Iquique Canal 10.strm Added Chile/Telecanal.strm Added folder Colombia Added Colombia/Canal 41 ABN.strm Added Colombia/Teleantioquia.strm Added folder Cuba Added Cuba/Cubavision.strm Added folder Dominican Republic Added Dominican Republic/Vega TV.strm Added folder El Salvador Added El Salvador/Canal 21.strm Added folder Honduras Added Honduras/Canal 6 News.strm Added folder Mexico Added Mexico/Grupo FM TV.strm Added Mexico/Mexiquense.strm Added folder Panama Added Panama/TVN.strm Added folder Venezuela Added Venezuela/VIVE TV.strm

--- trunk/BUILD.bat
+++ trunk/BUILD.bat
@@ -6,16 +6,24 @@ rmdir XBMC-TV /S /Q
 ECHO ------------------------------
 ECHO Building XBMC-TV Directory...
 xcopy "Andorra" "XBMC-TV\Andorra" /E /Q /I /Y
+xcopy "Antigua and Barbados" "XBMC-TV\Antigua and Barbados" /E /Q /I /Y
+xcopy "Argentina" "XBMC-TV\Argentina" /E /Q /I /Y
 xcopy "Armenia" "XBMC-TV\Armenia" /E /Q /I /Y
 xcopy "Austria" "XBMC-TV\Austria" /E /Q /I /Y
 xcopy "Australia" "XBMC-TV\Australia" /E /Q /I /Y
 xcopy "Brazil" "XBMC-TV\Brazil" /E /Q /I /Y
 xcopy "Canada" "XBMC-TV\Canada" /E /Q /I /Y
+xcopy "Chile" "XBMC-TV\Chile" /E /Q /I /Y
+xcopy "Colombia" "XBMC-TV\Colombia" /E /Q /I /Y
+xcopy "Cuba" "XBMC-TV\Cuba" /E /Q /I /Y
 xcopy "Czech Republic" "XBMC-TV\Czech Republic" /E /Q /I /Y
 xcopy "Denmark" "XBMC-TV\Denmark" /E /Q /I /Y
+xcopy "Dominican Republic" "XBMC-TV\Dominican Republic" /E /Q /I /Y
+xcopy "El Salvador" "XBMC-TV\El Salvador" /E /Q /I /Y
 xcopy "Estonia" "XBMC-TV\Estonia" /E /Q /I /Y
 xcopy "France" "XBMC-TV\France" /E /Q /I /Y
 xcopy "Germany" "XBMC-TV\Germany" /E /Q /I /Y
+xcopy "Honduras" "XBMC-TV\Honduras" /E /Q /I /Y
 xcopy "Hungary" "XBMC-TV\Hungary" /E /Q /I /Y
 xcopy "Italy" "XBMC-TV\Italy" /E /Q /I /Y
 xcopy "Latvia" "XBMC-TV\Latvia" /E /Q /I /Y
@@ -23,6 +31,8 @@ xcopy "Lithuania" "XBMC-TV\Lithuania" /E /Q /I /Y
 xcopy "Japan" "XBMC-TV\Japan" /E /Q /I /Y
 xcopy "Luxembourg" "XBMC-TV\Luxembourg" /E /Q /I /Y
 xcopy "Netherlands" "XBMC-TV\Netherlands" /E /Q /I /Y
+xcopy "Mexico" "XBMC-TV\Mexico" /E /Q /I /Y
+xcopy "Panama" "XBMC-TV\Panama" /E /Q /I /Y
 xcopy "Poland" "XBMC-TV\Poland" /E /Q /I /Y
 xcopy "Portugal" "XBMC-TV\Portugal" /E /Q /I /Y
 xcopy "Romania" "XBMC-TV\Romania" /E /Q /I /Y
@@ -34,6 +44,7 @@ xcopy "Sweden" "XBMC-TV\Sweden" /E /Q /I /Y
 xcopy "Switzerland" "XBMC-TV\Switzerland" /E /Q /I /Y
 xcopy "United Kingdom" "XBMC-TV\United Kingdom" /E /Q /I /Y
 xcopy "United States" "XBMC-TV\United States" /E /Q /I /Y
+xcopy "Venezuela" "XBMC-TV\Venezuela" /E /Q /I /Y
 xcopy "Webcams" "XBMC-TV\Webcams" /E /Q /I /Y
 
 ECHO ------------------------------

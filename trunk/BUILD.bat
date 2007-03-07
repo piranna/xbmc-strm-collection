ca5f755c03955bcc889ccae772439a7e33c7a03f
akaigotchi <akaigotchi@39f0f031-5427-0410-9fd6-272d3ce43485>
2007-03-07 00:32:38 +0000

    Updated BUILD.bat to work with newly added countries Added China (thx to ElPiranna)

--- trunk/BUILD.bat
+++ trunk/BUILD.bat
@@ -15,6 +15,7 @@ xcopy "Belgium" "XBMC-TV\Belgium" /E /Q /I /Y
 xcopy "Brazil" "XBMC-TV\Brazil" /E /Q /I /Y
 xcopy "Canada" "XBMC-TV\Canada" /E /Q /I /Y
 xcopy "Chile" "XBMC-TV\Chile" /E /Q /I /Y
+xcopy "China" "XBMC-TV\China" /E /Q /I /Y
 xcopy "Colombia" "XBMC-TV\Colombia" /E /Q /I /Y
 xcopy "Cuba" "XBMC-TV\Cuba" /E /Q /I /Y
 xcopy "Czech Republic" "XBMC-TV\Czech Republic" /E /Q /I /Y
@@ -24,16 +25,20 @@ xcopy "El Salvador" "XBMC-TV\El Salvador" /E /Q /I /Y
 xcopy "Estonia" "XBMC-TV\Estonia" /E /Q /I /Y
 xcopy "France" "XBMC-TV\France" /E /Q /I /Y
 xcopy "Germany" "XBMC-TV\Germany" /E /Q /I /Y
+xcopy "Greece" "XBMC-TV\Greece" /E /Q /I /Y
 xcopy "Honduras" "XBMC-TV\Honduras" /E /Q /I /Y
 xcopy "Hungary" "XBMC-TV\Hungary" /E /Q /I /Y
 xcopy "India" "XBMC-TV\India" /E /Q /I /Y
 xcopy "Italy" "XBMC-TV\Italy" /E /Q /I /Y
+xcopy "Japan" "XBMC-TV\Japan" /E /Q /I /Y
+xcopy "Korea" "XBMC-TV\Korea" /E /Q /I /Y
 xcopy "Latvia" "XBMC-TV\Latvia" /E /Q /I /Y
 xcopy "Lithuania" "XBMC-TV\Lithuania" /E /Q /I /Y
-xcopy "Japan" "XBMC-TV\Japan" /E /Q /I /Y
 xcopy "Luxembourg" "XBMC-TV\Luxembourg" /E /Q /I /Y
-xcopy "Netherlands" "XBMC-TV\Netherlands" /E /Q /I /Y
+xcopy "Malaysia" "XBMC-TV\Malaysia" /E /Q /I /Y
+xcopy "Malta" "XBMC-TV\Malta" /E /Q /I /Y
 xcopy "Mexico" "XBMC-TV\Mexico" /E /Q /I /Y
+xcopy "Netherlands" "XBMC-TV\Netherlands" /E /Q /I /Y
 xcopy "Panama" "XBMC-TV\Panama" /E /Q /I /Y
 xcopy "Poland" "XBMC-TV\Poland" /E /Q /I /Y
 xcopy "Portugal" "XBMC-TV\Portugal" /E /Q /I /Y
@@ -42,8 +47,11 @@ xcopy "Russia" "XBMC-TV\Russia" /E /Q /I /Y
 xcopy "Slovakia" "XBMC-TV\Slovakia" /E /Q /I /Y
 xcopy "Slovenia" "XBMC-TV\Slovenia" /E /Q /I /Y
 xcopy "Spain" "XBMC-TV\Spain" /E /Q /I /Y
+xcopy "Sri Lanka" "XBMC-TV\Sri Lanka" /E /Q /I /Y
 xcopy "Sweden" "XBMC-TV\Sweden" /E /Q /I /Y
 xcopy "Switzerland" "XBMC-TV\Switzerland" /E /Q /I /Y
+xcopy "Thailand" "XBMC-TV\Thailand" /E /Q /I /Y
+xcopy "Turkey" "XBMC-TV\Turkey" /E /Q /I /Y
 xcopy "Ukraine" "XBMC-TV\Ukraine" /E /Q /I /Y
 xcopy "United Kingdom" "XBMC-TV\United Kingdom" /E /Q /I /Y
 xcopy "United States" "XBMC-TV\United States" /E /Q /I /Y

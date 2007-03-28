15ce0ce229a417237b5f04896d0ec6ba0277aeff
eleniontolto <eleniontolto@39f0f031-5427-0410-9fd6-272d3ce43485>
2007-03-28 07:46:48 +0000

    [MC360 Mod] Changed: Added Wide Marketplace Icons to BUILD.bat.  Changed:  Fixed typo in Install.txt

--- MC360 v1.x Video Marketplace Mod/BUILD.bat
+++ MC360 v1.x Video Marketplace Mod/BUILD.bat
@@ -6,6 +6,7 @@ rmdir BUILD /S /Q
 ECHO ------------------------------
 ECHO Building BUILD Directory...
 xcopy "Marketplace Icons" "BUILD\Marketplace Icons" /E /Q /I /Y
+xcopy "Marketplace Icons" "BUILD\Wide Marketplace Icons" /E /Q /I /Y
 xcopy "XBMC" "BUILD\XBMC" /E /Q /I /Y
 
 ECHO ------------------------------

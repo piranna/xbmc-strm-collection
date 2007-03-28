80a2f45e97d1b21a6610b820d55f8a6ada3c112d
akaigotchi <akaigotchi@39f0f031-5427-0410-9fd6-272d3ce43485>
2007-03-28 16:48:00 +0000

    Modified: BUILD.bat now has two separate build options, one for building with wide icons, one for building with big icons Modified: INSTALL.txt is now changed according to new build options in BUILD.bat

--- MC360 v1.x Video Marketplace Mod/BUILD.bat
+++ MC360 v1.x Video Marketplace Mod/BUILD.bat
@@ -4,16 +4,54 @@ ECHO Creating "BUILD" Folder
 rmdir BUILD /S /Q
 
 ECHO ------------------------------
-ECHO Building BUILD Directory...
-xcopy "Marketplace Icons" "BUILD\Marketplace Icons" /E /Q /I /Y
-xcopy "Marketplace Icons" "BUILD\Wide Marketplace Icons" /E /Q /I /Y
-xcopy "XBMC" "BUILD\XBMC" /E /Q /I /Y
+ECHO Please enter your build option 
 
+ECHO (1) Build with Wide Icons
+ECHO (2) Build with Big Icons
+
+SET /p SELECTION=Enter the number of your build option and press enter:
+
+IF "%SELECTION%"=="1" GOTO WIDE_ICONS
+IF "%SELECTION%"=="2" GOTO BIG_ICONS
+
+:WIDE_ICONS
+ECHO ------------------------------
+ECHO Building with Wide Icons
+ECHO ------------------------------
+
+ECHO Copying content..
+xcopy "Wide Marketplace Icons" "BUILD\Marketplace Icons" /E /Q /I /Y
+GOTO MAIN
+
+
+:BIG_ICONS
+ECHO ------------------------------
+ECHO Building with Big Icons
+ECHO ------------------------------
+
+ECHO Copying content..
+
+xcopy "Marketplace Icons" "BUILD\Marketplace Icons" /E /Q /I /Y
+GOTO MAIN
+
+
+:MAIN
+xcopy "XBMC" "BUILD\XBMC" /E /Q /I /Y
+GOTO CLEANUP
+
+
+:CLEANUP
 ECHO ------------------------------
 ECHO Removing SVN directories from BUILD...
 FOR /R BUILD %%d IN (SVN) DO @RD /S /Q "%%d" 2>NUL
 
 ECHO Complete
-
-
 pause
+
+
+
+
+
+
+
+

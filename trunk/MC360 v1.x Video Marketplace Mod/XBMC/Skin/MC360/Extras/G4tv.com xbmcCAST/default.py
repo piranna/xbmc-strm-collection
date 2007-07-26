0ad038a6563f0a33df69b029fb2cda65cd8e68e5
eleniontolto <eleniontolto@39f0f031-5427-0410-9fd6-272d3ce43485>
2007-07-26 09:49:13 +0000

    [MC360 Mod] Added: Option to automatically start partymode at startup of XBMC Added: Option to remove home blade logos. Added: Option to remove home blade labels Added: Option to remove home blade button icons (the little images of the controller buttons at the bottom of the screen) Fixed: some spelling errors and mislabelings. Removed: T3ch Downloader no longer included with mod, since it is included with all T3ch builds now. Added: Submenu Mod for games blade, since XBMC glitch that was giving me problems has finally been fixed. Added/Changed/Removed/Updated: Probably a lot of things I don't remember, since it's been about 3 months since I last uploaded anything.

--- trunk/MC360 v1.x Video Marketplace Mod/XBMC/Skin/MC360/Extras/G4tv.com xbmcCAST/default.py
+++ trunk/MC360 v1.x Video Marketplace Mod/XBMC/Skin/MC360/Extras/G4tv.com xbmcCAST/default.py
@@ -1,4 +1,4 @@
-#G4.py Version 0.1
+#G4.py Version 0.3
 #Author Greg Chrystall greg@chrystall.co.nz
 #Script modded by Brad Quesnell 8bitbrad@gmail.com
 #Note this is the first release, heavily copied from other XBMC Scripts.
@@ -22,17 +22,6 @@ ACTION_PAUSE            = 12
 ACTION_STOP             = 13
 ACTION_NEXT_ITEM        = 14
 ACTION_PREV_ITEM        = 15
-
-COORD_1080I      = 0 
-COORD_720P       = 1 
-COORD_480P_4X3   = 2 
-COORD_480P_16X9  = 3 
-COORD_NTSC_4X3   = 4 
-COORD_NTSC_16X9  = 5 
-COORD_PAL_4X3    = 6 
-COORD_PAL_16X9   = 7 
-COORD_PAL60_4X3  = 8 
-COORD_PAL60_16X9 = 9
 
 # dialog object for the whole app
 dialog = xbmcgui.DialogProgress()
@@ -62,123 +51,96 @@ rssFeeds                = {'Around the Net' : 'http://www.g4tv.com/dailynut/podc
                            'AOTS: In Your Pants' : 'http://www.g4tv.com/attackoftheshow/podcasts/22/AOTS_In_Your_Pants.xml',
                            'AOTS: Hardware' : 'http://www.g4tv.com/attackoftheshow/podcasts/23/AOTS_Hardware.xml',
                            'AOTS: Game Break' : 'http://www.g4tv.com/attackoftheshow/podcasts/24/AOTS_Game_Break.xml',
-                           'The Block Podcast' : 'http://www.g4tv.com/theblock/podcasts/20/The_Block_Podcast.xml',
                            'G4tv.com Originals' : 'http://www.g4tv.com/g4originals/podcasts/25/G4tvcom_Originals.xml',
                            'Cheat!':'http://www.g4tv.com/cheat/podcasts/7/Cheat_Video_Podcast.xml',
                            'Cinematech':'http://www.g4tv.com/cinematech/podcasts/8/Cinematech_Video_Podcast.xml',
                            'Cinematech : Nocturnal Emissions':'http://www.g4tv.com/cinematechnocturnalemissions/podcasts/9/Cinematech_Nocturnal_Emissions_Video_Podcast.xml',
-                           'Filter':'http://www.g4tv.com/filter/podcasts/10/Filter_Video_Podcast.xml',
-                           'Formula D':'http://www.g4tv.com/formulad/podcasts/18/Formula_D_Video_Podcast.xml',
                            'Happy Tree Friends':'http://podcast.happytreefriends.com/htfrss.xml',
-                           'Icons':'http://www.g4tv.com/icons/podcasts/17/ICONS_Video_Podcast.xml',
+                           'ICONS':'http://www.g4tv.com/icons/podcasts/17/ICONS_Video_Podcast.xml',
                            'Street Fury':'http://www.g4tv.com/streetfury/podcasts/11/Street_Fury_Video_Podcast.xml',
-                           'The Feed':'http://www.g4tv.com/thefeed/podcasts/19/The_Daily_Feed_Video_Podcast.xml',
+                           'The Daily Feed':'http://www.g4tv.com/thefeed/podcasts/19/The_Daily_Feed_Video_Podcast.xml',
                            'The Man Show':'http://www.g4tv.com/themanshow/podcasts/14/The_Man_Show_Video_Podcast.xml',
-                           'X-Play':'http://www.g4tv.com/xplay/podcasts/6/XPlay_Daily_Video_Podcast.xml'
+                           'X-Play':'http://www.g4tv.com/xplay/podcasts/6/XPlay_Daily_Video_Podcast.xml',
+                           'Ninja Warrior':'http://www.g4tv.com/ninjawarrior/podcasts/27/Ninja_Warrior_Video_Podcast.xml',
+                           'Freestyle 101':'http://www.g4tv.com/g4originals/podcasts/26/Freestyle_101_Video_Podcast.xml'
                            }
                            
 class G4Viewer(xbmcgui.Window):
         def __init__(self):
-                self.setCoordinateResolution(COORD_NTSC_4X3)
                 if Emulating: xbmcgui.Window.__init__(self)
                 dialog.create("Getting RSS Feeds")
                 self.feeds = {}
                 self.urlsInOrder = []
-                W = self.getWidth()
-                H = self.getHeight()
-                LOG("Image: " + IMAGE_DIR + 'background.png')
-        	# background animation
-                self.imgBackgroundani = xbmcgui.ControlImage(0, 0, 720, 476, "Q:\\Skin\\MC360\\Extras\\default-ani.gif")
-                self.addControl(self.imgBackgroundani)
-            
-        	# background image
-		self.imgBackgroundBlue = xbmcgui.ControlImage(0, 0, 720, 576, 'background-blue-alpha.png')
-                self.addControl(self.imgBackgroundBlue)
-                # custom background image
-                self.imgCustomBackground = xbmcgui.ControlImage(0, 0, 720, 576, xbmc.getInfoLabel('Skin.String(Media)'))
-                self.addControl(self.imgCustomBackground)
-                self.imgCustomBackground.setColorDiffuse('D1FFFFFF')
-
-        	# Whitewash glass top left
-        	self.imgWhiteTL = xbmcgui.ControlImage(70, 0, 16, 64, 'bkgd-whitewash-glass-top-left.png')
-        	self.addControl(self.imgWhiteTL)
-
-        	# Whitewash glass top middle
-        	self.imgWhiteTMID = xbmcgui.ControlImage(86, 0, 592, 64, 'bkgd-whitewash-glass-top-middle.png')
-        	self.addControl(self.imgWhiteTMID)
-
-        	# Whitewash glass top right
-        	self.imgWhiteTR = xbmcgui.ControlImage(678, 0, 16, 64, 'bkgd-whitewash-glass-top-right.png')
-        	self.addControl(self.imgWhiteTR)
-
-        	# Whitewash glass bottom left
-        	self.imgWhiteBL = xbmcgui.ControlImage(70, 412, 16, 64, 'bkgd-whitewash-glass-bottom-left.png')
-        	self.addControl(self.imgWhiteBL)
-
-        	# Whitewash glass bottom middle
-        	self.imgWhiteBMID = xbmcgui.ControlImage(86, 412, 592, 64, 'bkgd-whitewash-glass-bottom-middle.png')
-        	self.addControl(self.imgWhiteBMID)
-
-        	# Whitewash glass bottom right
-        	self.imgWhiteBR = xbmcgui.ControlImage(678, 412, 16, 64, 'bkgd-whitewash-glass-bottom-right.png')
-        	self.addControl(self.imgWhiteBR)
-
-        	# Whitewash overlay left
-        	self.imgWhitewashL = xbmcgui.ControlImage(60, 0, 32, 476, 'background-overlay-whitewash-left.png')
-        	self.addControl(self.imgWhitewashL)
-
-        	# Whitewash overlay middle
-        	self.imgWhitewashMID = xbmcgui.ControlImage(92, 0, 553, 476, 'background-overlay-whitewash-centertile.png')
-        	self.addControl(self.imgWhitewashMID)
-
-        	# Whitewash overlay right
-        	self.imgWhitewashR = xbmcgui.ControlImage(645, 0, 64, 476, 'background-overlay-whitewash-right.png')
-        	self.addControl(self.imgWhitewashR)
-
-        	# Left blade runner
-        	self.imgBladerunL = xbmcgui.ControlImage(-61, 0, 128, 476, 'blades-runner-left.png')
-        	self.addControl(self.imgBladerunL)
-
-        	# Right blade runner
-        	self.imgBladerunR = xbmcgui.ControlImage(665, 0, 128, 476, 'blades-runner-right.png')
-        	self.addControl(self.imgBladerunR)
-
-        	# Header Blade
-        	self.imgBlade = xbmcgui.ControlImage(18, 0, 80, 476, 'blades-size4-header.png')
-        	self.addControl(self.imgBlade)
-
-        	# Y Button
-        	self.imgybutton = xbmcgui.ControlImage(125, 420, 21, 21, 'button-Y-turnedoff.png')
-        	self.addControl(self.imgybutton)
-
-        	# X Button
-        	self.imgxbutton = xbmcgui.ControlImage(112, 440, 21, 21, 'button-X.png')
-        	self.addControl(self.imgxbutton)
-
-        	# Back Button
-        	self.imgbackbutton = xbmcgui.ControlImage(620, 420, 21, 21, 'button-back.png')
-        	self.addControl(self.imgbackbutton)
-
-        	# A Button
-        	self.imgabutton = xbmcgui.ControlImage(633, 440, 21, 21, 'button-A.png')
-        	self.addControl(self.imgabutton)
-
-                self.medialabel           = xbmcgui.ControlLabel(79,129,100,20,'media','font14','0xFF000000',angle=270)
-                self.g4label           = xbmcgui.ControlLabel(95,30,375,20,'G4tv.com xbmcCast','font18','0xFFFFFFFF')
-                self.vislabel           = xbmcgui.ControlLabel(145,443,375,20,'Full Screen Visualization','font12','0xFFFFFFFF')
-                self.backlabel           = xbmcgui.ControlLabel(580,423,80,20,'Back','font12','0xFFFFFFFF')
-                self.selectlabel           = xbmcgui.ControlLabel(577,443,80,20,'Select','font12','0xFFFFFFFF')
                 
+                #xbmcgui.lock()
+
+		if xbmc.getSkinDir() == 'MC360':
+			# Get scaling info
+			self.scaleX = ( float(self.getWidth())  / float(720) )
+			self.scaleY = ( float(self.getHeight()) / float(480) )
+
+			# Create static controls
+			self.addControl(xbmcgui.ControlImage(0,0, int(720*self.scaleX),int(480*self.scaleY), 'background-blue.png'))
+			self.addControl(xbmcgui.ControlImage(18,0, int(720*self.scaleX),int(480*self.scaleY), xbmc.getInfoLabel('Skin.String(Media)')))
+			self.addControl(xbmcgui.ControlImage(int(70*self.scaleX),0, int(16*self.scaleX),int(54*self.scaleY), 'bkgd-whitewash-glass-top-left.png'))
+			self.addControl(xbmcgui.ControlImage(int(86*self.scaleX),0, int(667*self.scaleX),int(54*self.scaleY), 'bkgd-whitewash-glass-top-middle.png'))
+			self.addControl(xbmcgui.ControlImage(int(753*self.scaleX),0, int(16*self.scaleX),int(54*self.scaleY), 'bkgd-whitewash-glass-top-right.png'))
+			self.addControl(xbmcgui.ControlImage(int(86*self.scaleX),int(427*self.scaleY), int(667*self.scaleX),int(54*self.scaleY), 'bkgd-whitewash-glass-bottom-middle.png'))
+			self.addControl(xbmcgui.ControlImage(int(70*self.scaleX),int(427*self.scaleY), int(16*self.scaleX),int(54*self.scaleY), 'bkgd-whitewash-glass-bottom-left.png'))
+			self.addControl(xbmcgui.ControlImage(int(753*self.scaleX),int(427*self.scaleY), int(667*self.scaleX),int(54*self.scaleY), 'bkgd-whitewash-glass-bottom-right.png'))
+			self.addControl(xbmcgui.ControlImage(int(60*self.scaleX),0, int(32*self.scaleX),int(480*self.scaleY), 'background-overlay-whitewash-left.png'))
+			self.addControl(xbmcgui.ControlImage(int(92*self.scaleX),0, int(628*self.scaleX),int(480*self.scaleY), 'background-overlay-whitewash-centertile.png'))
+			self.addControl(xbmcgui.ControlImage(int(-61*self.scaleX),0, int(128*self.scaleX),int(480*self.scaleY), 'blades-runner-left.png'))
+			self.addControl(xbmcgui.ControlImage(int(18*self.scaleX),0, int(80*self.scaleX),int(480*self.scaleY), 'blades-size4-header.png'))
+			self.addControl(xbmcgui.ControlLabel(int(155*self.scaleX),int(35*self.scaleY), int(200*self.scaleX),int(35*self.scaleY), 'xbmcCAST', font="font18"))
+			self.addControl(xbmcgui.ControlLabel(int(79*self.scaleX),int(129.1666*self.scaleY), int(270*self.scaleX),int(35*self.scaleY), 'media',angle=270,textColor="0xFF000000",font="font14"))
+			
+
+			# Create button images and descriptions
+			
+			#self.y_img = xbmcgui.ControlImage(int(125*self.scaleX),int(420*self.scaleY), int(21*self.scaleX),int(18*self.scaleY), 'button-Y.png')
+			#self.addControl(self.y_img)
+			#self.y_desc = xbmcgui.ControlLabel(int(155*self.scaleX),int(420*self.scaleY), int(200*self.scaleX),int(35*self.scaleY), 'Action 2',font="font12")
+			#self.addControl(self.y_desc)
+			#self.y_on = 1
+
+			self.x_img = xbmcgui.ControlImage(int(110*self.scaleX),int(440*self.scaleY), int(21*self.scaleX),int(18*self.scaleY), 'button-Back.png')
+			self.addControl(self.x_img)
+			self.x_desc = xbmcgui.ControlLabel(int(140*self.scaleX),int(440*self.scaleY), int(200*self.scaleX),int(35*self.scaleY), 'Exit',font="font12")
+			self.addControl(self.x_desc)
+			self.x_on = 1
+
+			#self.b_img = xbmcgui.ControlImage(int(610*self.scaleX),int(420*self.scaleY), int(21*self.scaleX),int(18*self.scaleY), 'button-B-turnedoff.png')
+			#self.addControl(self.b_img)
+			#self.b_desc = xbmcgui.ControlLabel(int(600*self.scaleX),int(420*self.scaleY), int(200*self.scaleX),int(35*self.scaleY), 'Back',alignment=1,font="font12")
+			#self.addControl(self.b_desc)
+			#self.x_on = 0
+
+
+
+			self.a_img = xbmcgui.ControlImage(int(625*self.scaleX),int(440*self.scaleY), int(21*self.scaleX),int(18*self.scaleY), 'button-A.png')
+			self.addControl(self.a_img)
+			self.a_desc = xbmcgui.ControlLabel(int(615*self.scaleX),int(440*self.scaleY), int(200*self.scaleX),int(35*self.scaleY), 'Select',alignment=1,font="font12")
+			self.addControl(self.a_desc)
+
+			self.addControl(xbmcgui.ControlImage(90, 3, 60, 60,ROOT_DIR + 'default.tbn'))
+
+			# Create list
+			self.list = xbmcgui.ControlList(int(90*self.scaleX), int(70*self.scaleY), int(610*self.scaleX), int(340*self.scaleY),"font13","0xFF000000","iconlist-nofocus.png","iconlist-focus.png",itemTextXOffset=4,imageWidth=32, imageHeight=32,itemHeight=40)
+			self.addControl(self.list)
+		else:
+			# Get scaling info
+			self.scaleX = ( float(self.getWidth())  / float(720) )
+			self.scaleY = ( float(self.getHeight()) / float(576) )
+
+			#LIST
+
+                self.shows              = xbmcgui.ControlList(100,66,160,370,'font13','0xFF000000',itemTextXOffset=-10)
+                self.itemList           = xbmcgui.ControlList(270,66,420,370,'font13','0xFF000000')
                 
-                self.shows              = xbmcgui.ControlList(87,64,140,390,'font13','0xFF000000')
-                self.itemList           = xbmcgui.ControlList(290,64,375,390,'font13','0xFF000000')
-                self.addControl(self.medialabel)
-                self.addControl(self.g4label) 
-                self.addControl(self.vislabel)
-                self.addControl(self.backlabel)
-                self.addControl(self.selectlabel)               
                 self.addControl(self.shows)
                 self.addControl(self.itemList)
+
                 #Move left and right.
                 self.shows.controlRight(self.itemList)
                 self.shows.controlLeft(self.itemList)
